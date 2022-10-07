8a10daacc549fbd85cd40722f9a4dd96304e8ee4
Vít Kolář <kolavitek@gmail.com>
2022-10-07 16:31:25 +0200

    MachineDualTape class added

---
+++ TS/src/com/company/MachineDualTape.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class MachineDualTape extends Machine {
+    public MachineDualTape(Rule[] rules, Tape tape, int head, int finalState) {
+        super(rules, tape, head, finalState);
+    }
+
+}
